86a752a3d85ccb3d512215afc6676848ac8130ee
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-14 18:35:36 +0300

    add

---
+++ lesson1-selectors/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    // СЕЛЕКТОРЫ
+
+    // СЕЛЕКТОРЫ ПО ТЕГУ
+
+    // $("h1").addClass('selected');
+    // $("p").addClass('selected');
+    // $("h2").addClass('selected');
+    // $("ul").addClass('selected');
+    // $("li").addClass('selected');
+
+    // СЕЛЕКТОРЫ ПО #id
+
+    // $("#top-header").addClass('selected');
+    // $("#second-list-item").addClass('selected');
+    // $("#one-more-p").addClass('selected');
+
+    // СЕЛЕКТОРЫ ПО КЛАССУ
+
+    // $(".header-2").addClass('selected');
+    // $(".block").addClass('selected');
+    // $(".green").addClass('selected');
+
+    // КОМБИНИРОВАННЫЕ СЕЛЕКТОРЫ
+
+    // $("ul .green").addClass('selected');
+    // $("h2 ~ p").addClass('selected');
+    // $("h2 + p").addClass('selected');
+
+    // ПРОСТЫЕ ФИЛЬТРЫ
+
+    // $("p:first").addClass('selected');
+    // $("p:last").addClass('selected');
+    // $("p:odd").addClass('selected'); // четные параграфы
+    // $("p:even").addClass('selected'); // нечетные параграфы
+    // $("p:eq(0)").addClass('selected');
+    // $("p:eq(3)").addClass('selected');
+
+    // ФИЛЬТРЫ ПО СОДЕРЖИМОМУ
+
+    // $("h3:has(span)").addClass('selected');
+    // $(".block:parent").addClass('selected');
+    // $(".block:empty").addClass('selected');
+
+    // ФИЛЬТРЫ ПО АТТРИБУТАМ
+
+    // $("a").addClass('selected');
+    // $('a[href="http://google.ua"]').addClass('selected');
+    // $('a[href!="http://google.ua"]').addClass('selected');
+    // $('a[href^="http://"]').addClass('selected');
+    // $('a[href$=".ua"]').addClass('selected');
+
+});

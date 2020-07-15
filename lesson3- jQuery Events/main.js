7c932bcd4ec1c7f37f34bd9cb874eb2b3fdf41aa
Geadi <66213778+Geadi@users.noreply.github.com>
2020-07-15 20:53:46 +0300

    add

---
+++ lesson3- jQuery Events/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    // События в jQuery
+
+    /*
+
+    .click() - клик
+
+    .dblclick() двойной клик
+
+    .mouseenter() - мыш наведена
+
+    .mouseleave() - мыш отведена
+
+    .focus() - срабатывает когда есть фокус
+
+    .change() - срабатывает когда меняется 
+
+    .keydown() - нажатие клавиши
+
+    .keyup() - отжатие клавиши
+
+    .on("параметры", "функция") - тоже что и .click() но более универсальный
+
+    */
+
+    // $('#button-1').click( function () {
+    //     $("#top-header").toggleClass("pink")
+    // });
+
+    // $('#button-1').on('click', function (){
+    //     $("#top-header").toggleClass("pink")
+    // });
+
+    // $('#button-1').on('dblclick', function (){
+    //     $("#top-header").toggleClass("pink")
+    // });
+
+    // $('#testArea').on('mouseenter', function (){
+    //     $("#testArea").toggleClass("orangeBg")
+    // });
+
+    // $('#testArea').on('mouseleave', function (){
+    //     $("#testArea").toggleClass("orangeBg")
+    // });
+
+    // $('#inputName').on('focus', function (){
+    //     $("#inputName").addClass("focus")
+    // });
+
+    // $('#inputName').on('blur', function (){
+    //     $("#inputName").removeClass("focus")
+    // });
+
+    // $('#inputName').on('change', function (){
+    //     $("#textHolder").text( $('#inputName').val() )
+    // });
+
+    // $('#inputName').on('keyup', function (){
+    //     $("#textHolder").text( $('#inputName').val() )
+    // });
+
+    // $('#inputName').on('keyup', function (event){
+
+    //     if (event.which == 27) {
+
+    //     alert("esc")
+
+    // }
+
+    // });
+
+});
